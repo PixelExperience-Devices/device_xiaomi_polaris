568c4e1e5e2758f05b8df7260f09ef217fd8c295
Xhoni Rexhepi <xhonirexhepi@gmail.com>
2018-12-07 09:02:21 +0000

    polaris: Include Arcore

--- aosp_polaris.mk
+++ aosp_polaris.mk
@@ -13,6 +13,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_INCLUDE_STOCK_ARCORE := true 
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_polaris
