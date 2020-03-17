LineageOS/android_device_xiaomi_polaris

polaris: Split system and vendor props
Change-Id: If965be42e68e9ebbd816656dd2af79f541c1ceea

BoardConfig.mk
@@ -24,6 +24,10 @@ TARGET_USES_NQ_NFC := true
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
+# Properties
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 

PixelExperience-Devices/device_xiaomi_polaris

BoardConfig.mk
@@ -25,6 +25,10 @@ TARGET_USES_NQ_NFC := true
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 