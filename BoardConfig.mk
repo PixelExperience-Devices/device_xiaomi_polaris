0a92f824536997b63afd02334d2245e6e565d0a6
Chenyang Zhong <zhongcy95@gmail.com>
2020-06-18 17:17:27 -0400

    polaris: set Clang path to newly-tracked AOSP Clang master branch
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_KERNEL_SOURCE := kernel/xiaomi/polaris
 TARGET_KERNEL_CONFIG := polaris_defconfig
 TARGET_KERNEL_CLANG_VERSION := r383902
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86-master/clang-$(TARGET_KERNEL_CLANG_VERSION)
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
