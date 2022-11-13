201c74eb19cca0fbd1421ec187d80f374db6bfb3
Chenyang Zhong <zhongcy95@gmail.com>
2022-11-13 10:18:46 -0500

    polaris: shims: rename libpiex_shim to avoid conflict
    The current name has conflict with the one in hardware/custom/compat.
    I chose not to use that shim because mangling rules are not part of
    the c++ standard. A shim should always use the exposed symbol ABI
    directly instead of relying on the compiler to generate correct
    mangled symbols from c++ code.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -19,7 +19,7 @@ function blob_fixup() {
         patchelf --replace-needed "libstdc++.so"  "libc++.so" "${2}"
         ;;
     vendor/lib/camera/components/com.qti.node.watermark.so)
-        patchelf --add-needed "libpiex_shim.so" "${2}"
+        patchelf --add-needed "libpiex_shim_polaris.so" "${2}"
         ;;
     esac
 }
