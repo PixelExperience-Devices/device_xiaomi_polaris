ab6637b48d79d3321e08541f74ec3542655910bc
Chenyang Zhong <zhongcy95@gmail.com>
2022-12-13 10:00:55 -0500

    Revert "polaris: turn off compressed apex"
    Compressed apex boots fine after some recent kernel fixes.
    
    This reverts commit d2a4d5515036935971c4fd22ef39495a4933b904.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -7,8 +7,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-OVERRIDE_PRODUCT_COMPRESSED_APEX := false
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/polaris/polaris-vendor.mk)
 
